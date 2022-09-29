5f93b68d3122d1c9d9c3128df6a80fbd5bab3cbf
Artur <arturremizov@gmail.com>
2022-09-29 22:10:35 +0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Artur Remizov on 29.09.22.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
