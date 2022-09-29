80eae7c6c99b4090efff57588452184632a2b00c
Artur <arturremizov@gmail.com>
2022-09-29 23:09:59 +0300

    create a custom data model for Todo Items

---
+++ TodoList/Models/ItemModel.swift
@@ -0,0 +1,14 @@
+//
+//  ItemModel.swift
+//  TodoList
+//
+//  Created by Artur Remizov on 29.09.22.
+//
+
+import Foundation
+
+struct ItemModel: Identifiable {
+    var id: String = UUID().uuidString
+    let title: String
+    let isCompleted: Bool
+}
